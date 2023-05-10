34322b43957f5671955be9938e0d010f739f1823
Rajesh Kumar <Rajesh.jangra@gmail.com>
2023-05-10 18:23:41 +0530

    The first commit

---
+++ src/test/java/com/example/demo/archunit/LoggingTest.java
@@ -0,0 +1,21 @@
+package com.example.demo.archunit;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.CompositeArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.GeneralCodingRules.*;
+
+@AnalyzeClasses(packages = "com.example.demo")
+public class LoggingTest {
+
+    @ArchTest
+    private final ArchRule no_java_util_logging = NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
+
+    @ArchTest
+    static final ArchRule no_classes_should_access_standard_streams_or_throw_generic_exceptions =
+            CompositeArchRule.of(NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS).because("Using standard streams, should use loggers");
+}
---
+++ src/test/java/com/example/demo/archunit/StreamsTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.archunit;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.GeneralCodingRules.ACCESS_STANDARD_STREAMS;
+import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
+import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
+
+@AnalyzeClasses(packages = "com.example.demo")
+public class StreamsTest {
+
+    @ArchTest
+    private final ArchRule no_access_to_standard_streams = NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
+
+    @ArchTest
+    private void no_access_to_standard_streams_as_method(JavaClasses classes) {
+        noClasses().should(ACCESS_STANDARD_STREAMS).check(classes);
+    }
+
+    @ArchTest
+    private final ArchRule no_generic_exceptions = NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
+}
